ca0d0ecd2d79de7585936f618741106bbad92a03
DiamondDagger590 <diamonddagger590@gmail.com>
2025-01-22 16:44:41 -0500

    Add disabled worlds to skill level up

--- src/main/java/us/eunoians/mcrpg/listener/skill/SkillListener.java
+++ src/main/java/us/eunoians/mcrpg/listener/skill/SkillListener.java
@@ -12,13 +12,29 @@ import us.eunoians.mcrpg.skill.SkillRegistry;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * A generic listener that provides base implementation for any {@link Event} that
+ * can be used to level up a {@link us.eunoians.mcrpg.skill.Skill}.
+ */
 public interface SkillListener extends Listener {
 
+    /**
+     * Attempts to pass in the provided {@link Event} to all {@link us.eunoians.mcrpg.skill.Skill}s owned
+     * by the provided {@link UUID} to be parsed for leveling.
+     *
+     * @param uuid  The {@link UUID} of the {@link SkillHolder} to attempt to level skills for.
+     * @param event The {@link Event} being passed in for leveling.
+     */
     default void levelSkill(@NotNull UUID uuid, @NotNull Event event) {
         McRPG mcRPG = McRPG.getInstance();
         EntityManager entityManager = mcRPG.getEntityManager();
         SkillRegistry skillRegistry = mcRPG.getSkillRegistry();
         entityManager.getAbilityHolder(uuid).ifPresent(abilityHolder -> {
+
+            // Validate that the holder specific context allows for McRPG to be used here.
+            if (!mcRPG.getWorldManager().isMcRPGEnabledForHolder(abilityHolder)) {
+                return;
+            }
 
             if (abilityHolder instanceof SkillHolder skillHolder) {
                 Set<NamespacedKey> allSkills = skillHolder.getSkills();
